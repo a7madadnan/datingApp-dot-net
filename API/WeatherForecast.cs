e270d684beb2615198a8195e547445dcd185c7bf
Ahmad Ali <aa1904753@gmail.com>
2023-07-27 21:31:38 +0300

    install entityframework

--- API/WeatherForecast.cs
+++ API/WeatherForecast.cs
@@ -13,7 +13,7 @@ public class WeatherForecast
 
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-    public string? Summary { get; set; }
+    public string Summary { get; set; }
 
 }
 public class Car 
