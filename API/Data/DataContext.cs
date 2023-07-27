80074f57084acc29c6cdd4ed04caaefd615ecd23
Ahmad Ali <aa1904753@gmail.com>
2023-07-27 22:04:18 +0300

    create dataContext

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -2,11 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.data
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
-        
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
     }
 }
